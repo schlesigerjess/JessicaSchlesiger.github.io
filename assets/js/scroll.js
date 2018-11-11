67be6796dfa0e1ceb3e0954b3022e18fab296364
WhatAKerfuffle <Schlesigerjess@gmail.com>
2018-11-11 15:19:48 -0500

    Added fade in for index's contact form

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -3,12 +3,21 @@
 $(window).scroll(function() {
 	var windowHeight = $(window).height(),
 	windowScroll = $(this).scrollTop(),
-	buffer=50;
+	buffer=0;
+
+	var contactForm = $('.index-contact-form').offset().top;
+	console.log((contactForm-windowHeight), windowScroll);
+	if (windowScroll-buffer > (contactForm-windowHeight)){	
+		$('.index-contact-form').animate({
+			"opacity": "1",
+			"margin-top": "0px",
+			"margin-bottom": "0px"
+			}, 500);
+	}
 	
 	{% for skills in site.data.settings.skills %}
 		var skillsBar{{ skills.title | slugify }} = $('.skill-icons-{{ skills.title | slugify }}').offset().top;
-		console.log((skillsBar{{ skills.title | slugify }}-windowHeight) , windowScroll);
-		
+		console.log((skillsBar{{ skills.title | slugify }}-windowHeight), windowScroll);
 		if (windowScroll-buffer > (skillsBar{{ skills.title | slugify }}-windowHeight)){
 			moveSkillBar('.skill-icons-{{ skills.title | slugify }}');
 		}
